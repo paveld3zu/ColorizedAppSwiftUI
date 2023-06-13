ece2b3249797cc0f3a85f274be47effc8bbc0815
paveld3zu <pavel.karunnyj@yandex.ru>
2023-06-13 12:16:21 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Pavel Karunnyj on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
